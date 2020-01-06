54d7ae8d49de4f6306051ed677d6dcf8939efb29
Stéphane Fillion <s.fillion@ameos.com>
2020-01-06 14:17:24 +0100

    GraphiQL allowed for all application context

--- ext_tables.php
+++ ext_tables.php
@@ -3,7 +3,7 @@ defined('TYPO3_MODE') || die();
 
 call_user_func(
     function () use ($_EXTKEY) {
-        if (TYPO3_MODE == 'BE' && !\TYPO3\CMS\Core\Core\Environment::getContext()->isProduction()) {
+        if (TYPO3_MODE == 'BE') { // TODO : add ext_conf_template configuration
             \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
                 'Ameos.' . $_EXTKEY,
                 'web',
